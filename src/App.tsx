e1c988a90d476db77fe7b1c5f8e2e435bdf066f2
Rory Flint <rory.flint@tipalti.com>
2024-02-06 06:37:42 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div id="template-text">
+      <h1>React Starter Template - TypeScript</h1>
+      <p>
+        For JavaScript please use{" "}
+        <a href="https://github.com/ruairidhflint/react-template">this</a>{" "}
+        template
+      </p>
+    </div>
+  );
+}
+
+export default App;
